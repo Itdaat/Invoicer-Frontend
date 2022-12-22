0103dbed591197d75e6a8d1aca57ec54186f3ead
Duvie-Gazda <gazda.haimdavid@gmail.com>
2022-12-23 00:47:41 +0200

    added big title

---
+++ invoicer/src/lib/components/BigTitle.svelte
@@ -0,0 +1,21 @@
+
+
+<div class="container">
+    <slot></slot>
+</div>
+
+<style>
+    .container {
+        font-family: 'Poppins';
+        font-style: normal;
+        font-weight: 500;
+        font-size: 40px;
+        line-height: 60px;
+        /* identical to box height */
+
+        letter-spacing: 1px;
+
+        color: #3D5A80;
+
+    }
+</style>
