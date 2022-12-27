7463256724259de50c8283ed5bf7595b420c4fe9
Duvie-Gazda <gazda.haimdavid@gmail.com>
2022-12-27 12:25:23 +0200

    added login with google button

--- invoicer/src/translations/i18n.js
+++ invoicer/src/translations/i18n.js
@@ -3,6 +3,7 @@ import { register, init, getLocaleFromNavigator } from 'svelte-i18n';
 export function addLocalization() {
 
     register('en', () => import('./en.json'));
+    register('ua', () => import('./ua.json'));
     // en, en-US and pt are not available yet
 
     init({
