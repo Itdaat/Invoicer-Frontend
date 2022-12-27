4435e392591802d54562379c9d9e6ec132b39e65
Duvie-Gazda <gazda.haimdavid@gmail.com>
2022-12-28 01:45:49 +0200

    localization updated BH

---
+++ invoicer/src/routes/+layout.server.js
@@ -0,0 +1,23 @@
+
+/** @type {import('./$types').LayoutServerLoad} */
+export async function load({ cookies }) {
+  const userId = cookies.get('userId');
+
+  console.log(userId);
+
+  let currentLang = cookies.get('currentLand');
+  if (!currentLang) {
+    currentLang = 'en';
+    cookies.set('currentLang', currentLang);
+  }
+  let langData;
+  if (currentLang == 'en') {
+    langData = await import('../translations/en.json');
+  } else {
+    langData = await import('../translations/ua.json');
+  }
+  return {
+    userId: userId,
+    t: JSON.stringify(langData),
+  };
+}
