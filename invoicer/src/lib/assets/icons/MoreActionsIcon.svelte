7f320ef9a0a67b1aca394d249476e8a4cc3716bc
Duvie-Gazda <gazda.haimdavid@gmail.com>
2023-01-05 01:32:20 +0200

    added icons

---
+++ invoicer/src/lib/assets/icons/MoreActionsIcon.svelte
@@ -0,0 +1,82 @@
+<svg width="19" height="33" viewBox="0 0 19 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<g filter="url(#filter0_d_1_3600)">
+		<rect x="7" y="12" width="5" height="5" rx="2.5" transform="rotate(-90 7 12)" fill="#3D5A80" />
+	</g>
+	<g filter="url(#filter1_d_1_3600)">
+		<rect x="7" y="19" width="5" height="5" rx="2.5" transform="rotate(-90 7 19)" fill="#3D5A80" />
+	</g>
+	<g filter="url(#filter2_d_1_3600)">
+		<rect x="7" y="26" width="5" height="5" rx="2.5" transform="rotate(-90 7 26)" fill="#3D5A80" />
+	</g>
+	<defs>
+		<filter
+			id="filter0_d_1_3600"
+			x="0"
+			y="0"
+			width="19"
+			height="19"
+			filterUnits="userSpaceOnUse"
+			color-interpolation-filters="sRGB"
+		>
+			<feFlood flood-opacity="0" result="BackgroundImageFix" />
+			<feColorMatrix
+				in="SourceAlpha"
+				type="matrix"
+				values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+				result="hardAlpha"
+			/>
+			<feOffset />
+			<feGaussianBlur stdDeviation="3.5" />
+			<feComposite in2="hardAlpha" operator="out" />
+			<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.1 0" />
+			<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1_3600" />
+			<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1_3600" result="shape" />
+		</filter>
+		<filter
+			id="filter1_d_1_3600"
+			x="0"
+			y="7"
+			width="19"
+			height="19"
+			filterUnits="userSpaceOnUse"
+			color-interpolation-filters="sRGB"
+		>
+			<feFlood flood-opacity="0" result="BackgroundImageFix" />
+			<feColorMatrix
+				in="SourceAlpha"
+				type="matrix"
+				values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+				result="hardAlpha"
+			/>
+			<feOffset />
+			<feGaussianBlur stdDeviation="3.5" />
+			<feComposite in2="hardAlpha" operator="out" />
+			<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.1 0" />
+			<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1_3600" />
+			<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1_3600" result="shape" />
+		</filter>
+		<filter
+			id="filter2_d_1_3600"
+			x="0"
+			y="14"
+			width="19"
+			height="19"
+			filterUnits="userSpaceOnUse"
+			color-interpolation-filters="sRGB"
+		>
+			<feFlood flood-opacity="0" result="BackgroundImageFix" />
+			<feColorMatrix
+				in="SourceAlpha"
+				type="matrix"
+				values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+				result="hardAlpha"
+			/>
+			<feOffset />
+			<feGaussianBlur stdDeviation="3.5" />
+			<feComposite in2="hardAlpha" operator="out" />
+			<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.1 0" />
+			<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1_3600" />
+			<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1_3600" result="shape" />
+		</filter>
+	</defs>
+</svg>
