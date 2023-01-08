d2108a1e958ee344d1000bd0dfec02c147386af1
Duvie-Gazda <gazda.haimdavid@gmail.com>
2023-01-08 20:05:38 +0200

    added getting invoice for invoice page

--- invoicer/src/lib/api/CRM/invoice.js
+++ invoicer/src/lib/api/CRM/invoice.js
@@ -2,7 +2,7 @@
  * 
  * @param {string} firmId
  * @export
- * @returns {Promise<import('../../types/Entities').Invoice[]>}
+ * @returns {Promise<import('../../../types/Entities').Invoice[]>}
  */
 export async function getInvoices(firmId) {
     console.log(firmId);
@@ -25,7 +25,7 @@ export async function getInvoices(firmId) {
  * 
  * 
  * @export
- * @returns {Promise<import('../../types/Entities').InvoiceCounts>}
+ * @returns {Promise<import('../../../types/Entities').InvoiceCounts>}
  */
 export async function getInvoicesCount() {
     return {
@@ -33,5 +33,54 @@ export async function getInvoicesCount() {
         future: 10000,
         overdue: 300,
         soon: 125444
+    }
+}
+
+/**
+ * 
+ * 
+ * @export
+ * @returns {Promise<import('../../../types/Entities').Invoice>}
+ */
+export async function getInvoice() {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    let monthStr = month < 10 ? '0' + month : month;
+    let dayStr = day < 10 ? '0' + day : day;
+    const formedDate = `${dayStr}.${monthStr}.${year}`;
+    return {
+        creationDate: formedDate,
+        paymentDate: formedDate,
+        number: 'super',
+        order: {
+            currency: 'EUR',
+            downloadAddress: 'Makaka',
+            unloadAddress: 'Lupa',
+            myNumber: 'super',
+            price: 123.12,
+            paymentTerms: formedDate,
+            date: formedDate,
+            number: '123',
+            truck: {
+                number: "AA0000PO",
+                type: 'truck'
+            },
+            trailer: {
+                number: "OO9090PP",
+                type: 'trailer'
+            }
+        },
+        paymentTerms: formedDate,
+        status: 'missed',
+        cmr: {
+            image: '',
+            transport: {
+                number: '123',
+                type: 'trailer'
+            },
+            number: '123'
+        }
     }
 }
