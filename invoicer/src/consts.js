b9b907364cc05330688b37ee2372d7eafd6eb7ca
Duvie-Gazda <gazda.haimdavid@gmail.com>
2023-01-09 18:07:24 +0200

    added unreachable error const and type

--- invoicer/src/consts.js
+++ invoicer/src/consts.js
@@ -1,4 +1,4 @@
-export const ServerApiUrl = 'localhost:3000/';
+export const HostApiUrl = 'https://192.168.0.164:3000/';
 
 // ! res codes
 export const NOTFOUND = 404;
@@ -6,7 +6,10 @@ export const INTERNAL_SERVER_ERR = 500;
 export const FORBIDDEN_ERR = 403;
 export const OK_RES = 200;
 
+/** @typedef {{code : number , message : string}} responseError */
+
 // ! responses
+export const unreachableError = { code: 0, message: 'Server is not reachable' };
 export const userExists = { code: 1, message: "This login is already used" };
 export const notValidPassword = { code: 2, message: 'This password is not valid' };
 export const needRealLogin = { code: 3, message: 'You need to create new login' };
@@ -15,3 +18,7 @@ export const entityAlreadyExists = { code: 5, message: 'This entity already exis
 export const dataDuplication = { code: 6, message: 'Entity data duplication' };
 export const entityIsUsed = { code: 7, message: 'Entity is used' }
 export const notLatinSymbol = { code: 8, message: 'Some entity uses not latin symbols' };
+
+export const responseErrors = [
+    userExists, notValidPassword, needRealLogin, userNotFound, entityAlreadyExists, dataDuplication, entityIsUsed, notLatinSymbol
+]
