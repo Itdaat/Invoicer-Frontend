79dc8bdf9cf3e4d91a8e05144a82cdba3185c9b7
Duvie-Gazda <gazda.haimdavid@gmail.com>
2023-01-09 18:12:13 +0200

    added filters activation

--- invoicer/src/lib/mobile/components/MobileHeader.svelte
+++ invoicer/src/lib/mobile/components/MobileHeader.svelte
@@ -1,25 +1,56 @@
 <script>
+	import TransactionFilter from './filters/TransactionFilter.svelte';
+	import MessagesFilter from './filters/MessagesFilter.svelte';
+	import FilterPopup from './FilterPopup.svelte';
 	import { setDefaultFirm } from '$lib/api/CRM/firm';
 	import FirmStore from '$lib/stores/Firm';
 	import OpenFilterButton from './../../assets/icons/OpenFilterButton.svelte';
 	import OpenMenuIcon from './../../assets/icons/OpenMenuIcon.svelte';
 	import MobileHeaderMenu from './MobileHeaderMenu.svelte';
+	import SliderStore from '$lib/stores/Slides';
+	import InvoiceFilter from './filters/InvoiceFilter.svelte';
+	import OrderFilter from './filters/OrderFilter.svelte';
+	import PaymentFilter from './filters/PaymentFilter.svelte';
 
 	export let userId = '';
 
 	setDefaultFirm(userId);
 	$: firm = $FirmStore;
+
+	let showFilter = false;
+
+	const showFilterFun = () => {
+		showFilter = !showFilter;
+	};
+
+	$: slider = $SliderStore;
 </script>
 
 <div class="container">
 	<div class="top-panel">
 		<div class="menu"><OpenMenuIcon /></div>
 		<div class="title">{firm?.name}</div>
-		<div class="filter"><OpenFilterButton /></div>
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
+		<div class="filter" on:click={showFilterFun}><OpenFilterButton /></div>
 	</div>
 	<div class="pages-menu-horizontal">
 		<MobileHeaderMenu />
 	</div>
+	{#if showFilter}
+		<FilterPopup bind:showFilter>
+			{#if slider.activeIndex == 0}
+				<MessagesFilter />
+			{:else if slider.activeIndex == 1}
+				<InvoiceFilter />
+			{:else if slider.activeIndex == 2}
+				<OrderFilter />
+			{:else if slider.activeIndex == 3}
+				<PaymentFilter />
+			{:else if slider.activeIndex == 4}
+				<TransactionFilter />
+			{/if}
+		</FilterPopup>
+	{/if}
 </div>
 
 <style>
