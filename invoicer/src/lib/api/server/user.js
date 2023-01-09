3e8de31c27c6c01e193fbd48b7e1ea5a38df9bb5
Duvie-Gazda <gazda.haimdavid@gmail.com>
2023-01-09 18:08:22 +0200

    added login and checking login api

--- invoicer/src/lib/api/server/user.js
+++ invoicer/src/lib/api/server/user.js
@@ -1,3 +1,31 @@
-export async function loginUser() {
-    return await fetch('/api/reg', {});
+import { postRequestJson } from "$lib/helpers/apiHelper";
+
+/**
+ * 
+ * @export
+ * @param {string} userLogin 
+ * @param {string} userPassword 
+ * @returns 
+ */
+export async function loginUser(userLogin, userPassword) {
+    const reqBody = {
+        login: userLogin,
+        password: userPassword
+    };
+    const result = await postRequestJson('user/login', reqBody);
+    return result;
+}
+
+/**
+ * 
+ * 
+ * @export
+ * @param {string} userLogin 
+ * @returns 
+ */
+export async function checkUserLogin(userLogin) {
+    const result = await postRequestJson('user/check-login', {
+        login: userLogin
+    })
+    return result;
 }
