5b36ae674ed2a1b2fdcee00914404bd0ce31331e
Duvie-Gazda <gazda.haimdavid@gmail.com>
2023-01-10 19:01:44 +0200

    added arrow button

---
+++ invoicer/src/lib/assets/icons/ArrowButton.svelte
@@ -0,0 +1,30 @@
+<svg width="14" height="9" viewBox="0 0 14 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<rect
+		width="2.22231"
+		height="9.70967"
+		rx="1.11116"
+		transform="matrix(0.699282 -0.714845 0.699282 0.714845 0.161621 1.84421)"
+		fill="#3D5A80"
+	/>
+	<rect
+		width="2.22231"
+		height="9.70967"
+		rx="1.11116"
+		transform="matrix(0.699282 -0.714845 0.699282 0.714845 0.161621 1.84421)"
+		fill="#3D5A80"
+	/>
+	<rect
+		width="2.22231"
+		height="9.87904"
+		rx="1.11116"
+		transform="matrix(0.699282 0.714845 -0.699282 0.714845 12.446 0.250854)"
+		fill="#3D5A80"
+	/>
+	<rect
+		width="2.22231"
+		height="9.87904"
+		rx="1.11116"
+		transform="matrix(0.699282 0.714845 -0.699282 0.714845 12.446 0.250854)"
+		fill="#3D5A80"
+	/>
+</svg>
