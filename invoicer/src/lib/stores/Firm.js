35e2107d650875b86a384595e9fc06f61ae95331
Duvie-Gazda <gazda.haimdavid@gmail.com>
2023-01-10 13:02:44 +0200

    deleted firm store

--- invoicer/src/lib/stores/Firm.js
+++
@@ -1,9 +0,0 @@
-import { writable } from "svelte/store";
-
-/**
- * @type {import("svelte/store").Writable<import('../../types/Entities').Firm>}
- */
-const FirmStore = writable();
-
-export default FirmStore;
-
