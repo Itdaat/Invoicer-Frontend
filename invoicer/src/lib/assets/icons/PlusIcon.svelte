f77ec1d58d5eff2d0c601050199b57fffcec3536
Duvie-Gazda <gazda.haimdavid@gmail.com>
2023-01-10 13:05:37 +0200

    added icons

---
+++ invoicer/src/lib/assets/icons/PlusIcon.svelte
@@ -0,0 +1,20 @@
+<svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<rect x="12.439" width="5.12195" height="30" rx="2.56098" fill="#3D5A80" />
+	<rect x="12.439" width="5.12195" height="30" rx="2.56098" fill="#3D5A80" />
+	<rect
+		y="17.561"
+		width="5.12195"
+		height="30"
+		rx="2.56098"
+		transform="rotate(-90 0 17.561)"
+		fill="#3D5A80"
+	/>
+	<rect
+		y="17.561"
+		width="5.12195"
+		height="30"
+		rx="2.56098"
+		transform="rotate(-90 0 17.561)"
+		fill="#3D5A80"
+	/>
+</svg>
