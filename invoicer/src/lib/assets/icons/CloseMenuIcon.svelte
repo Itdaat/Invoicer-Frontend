bf1837843310ce9baeb553bea8ea3d64bc2b50e1
Duvie-Gazda <gazda.haimdavid@gmail.com>
2023-01-11 02:05:40 +0200

    added close menu icon

---
+++ invoicer/src/lib/assets/icons/CloseMenuIcon.svelte
@@ -0,0 +1,38 @@
+<svg width="33" height="33" viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<rect
+		x="7.3468"
+		y="10.0631"
+		width="3"
+		height="22.5"
+		rx="1.5"
+		transform="rotate(-45 7.3468 10.0631)"
+		fill="#3D5A80"
+	/>
+	<rect
+		x="7.3468"
+		y="10.0631"
+		width="3"
+		height="22.5"
+		rx="1.5"
+		transform="rotate(-45 7.3468 10.0631)"
+		fill="#3D5A80"
+	/>
+	<rect
+		x="10.0631"
+		y="25.973"
+		width="3"
+		height="22.5"
+		rx="1.5"
+		transform="rotate(-135 10.0631 25.973)"
+		fill="#3D5A80"
+	/>
+	<rect
+		x="10.0631"
+		y="25.973"
+		width="3"
+		height="22.5"
+		rx="1.5"
+		transform="rotate(-135 10.0631 25.973)"
+		fill="#3D5A80"
+	/>
+</svg>
