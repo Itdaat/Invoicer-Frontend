ecfe22375770456d0710161b5a6ddb2cf93822da
Duvie-Gazda <gazda.haimdavid@gmail.com>
2023-01-13 11:38:40 +0200

    icons added

---
+++ invoicer/src/lib/assets/icons/CheckGreenIcon.svelte
@@ -0,0 +1,16 @@
+<svg width="30" height="24" viewBox="0 0 30 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<rect
+		width="4.31082"
+		height="18.8636"
+		rx="2.15541"
+		transform="matrix(0.652586 -0.757714 0.652586 0.757714 0 9.30493)"
+		fill="#24A731"
+	/>
+	<rect
+		width="4.95108"
+		height="26.7231"
+		rx="2.47554"
+		transform="matrix(0.652586 0.757714 -0.652586 0.757714 26.769 0)"
+		fill="#24A731"
+	/>
+</svg>
