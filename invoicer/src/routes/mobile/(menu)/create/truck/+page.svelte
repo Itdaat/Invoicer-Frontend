3b46321f8d3f3b219e40500344cffe678b823a3a
Duvie-Gazda <gazda.haimdavid@gmail.com>
2023-01-13 13:04:49 +0200

    updated design

--- invoicer/src/routes/mobile/(menu)/create/truck/+page.svelte
+++ invoicer/src/routes/mobile/(menu)/create/truck/+page.svelte
@@ -10,20 +10,44 @@
 
 	$: t = $LanguageStore;
 
-	let license = '',
-		brand = '',
-		name = '';
+	/** @type {string | null}*/
+	let license = null;
+	/** @type {string | null}*/
+	let brand = null;
+	/** @type {string | null}*/
+	let name = null;
 
 	/**
 	 * @type {'ordinary' | 'success' | 'error'}
 	 */
 	let licenseStatus = 'ordinary';
 	/**
+	 * @type {'ordinary' | 'success' | 'error'}
+	 */
+	let nameStatus = 'ordinary';
+	/**
+	 * @type {'ordinary' | 'success' | 'error'}
+	 */
+	let brandStatus = 'ordinary';
+
+	/**
 	 * @type {string}
 	 */
 	let licenseText;
 
 	const save = async () => {
+		nameStatus = name ? 'ordinary' : 'error';
+		brandStatus = brand ? 'ordinary' : 'error';
+
+		if (!license) {
+			licenseText = t.transport_create_empty;
+			licenseStatus = 'error';
+		}
+
+		if (!name || !license || !brand) {
+			return;
+		}
+
 		const result = await createTruck(name, license, brand, TRANSPORT_TRUCK);
 		licenseStatus = 'ordinary';
 		if (result.error?.code == notLatinSymbol.code) {
@@ -37,22 +61,35 @@
 </script>
 
 <div class="main">
-	<!-- <MiniCategory title={t.transport_create_license}> -->
-	<LabeledInput bind:value={name} label={t.transport_create_name} placeHolder="super text" />
-	<LabeledInput
-		bind:value={license}
-		status={licenseStatus}
-		label={t.transport_create_license}
-		message={licenseText}
-		placeHolder="super text"
-	/>
-	<LabeledInput bind:value={brand} label={t.transport_create_brand} placeHolder="super text" />
-	<!-- </MiniCategory> -->
+	<MiniCategory title={t.transport_create_category}>
+		<LabeledInput
+			bind:value={name}
+			status={nameStatus}
+			label={t.transport_create_name}
+			message={t.transport_create_empty}
+			placeHolder={t.transport_create_name}
+		/>
+		<LabeledInput
+			bind:value={license}
+			status={licenseStatus}
+			label={t.transport_create_license}
+			message={licenseText}
+			placeHolder={t.transport_create_license}
+		/>
+		<LabeledInput
+			bind:value={brand}
+			status={brandStatus}
+			label={t.transport_create_brand}
+			placeHolder={t.transport_create_brand}
+			message={t.transport_create_empty}
+		/>
+	</MiniCategory>
 	<SaveEntity {save} />
 </div>
 
 <style>
 	.main {
 		width: 100%;
+		padding-top: 20px;
 	}
 </style>
