2ed60ced1d9896e2c6e92fcbef46e76068d46369
Duvie-Gazda <gazda.haimdavid@gmail.com>
2023-01-13 11:39:41 +0200

    added save entity button

---
+++ invoicer/src/lib/mobile/components/SaveEntity.svelte
@@ -0,0 +1,39 @@
+<script>
+	import CheckGreenIcon from './../../assets/icons/CheckGreenIcon.svelte';
+	import { slide } from 'svelte/transition';
+	import LanguageStore from './../../stores/Language.js';
+	import PlusIcon from './../../assets/icons/PlusIcon.svelte';
+	import { clickOutside } from '$lib/helpers/ClickOutside.js';
+	import { goto } from '$app/navigation';
+
+	export let save = () => {};
+</script>
+
+<main>
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
+	<div class="icon-container" on:click={save}>
+		<CheckGreenIcon />
+	</div>
+</main>
+
+<style>
+	main {
+		position: fixed;
+		right: 30px;
+		bottom: 50px;
+		z-index: 4;
+	}
+
+	.icon-container {
+		width: 60px;
+		height: 60px;
+		margin-top: 3px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		user-select: none;
+		background: #ffffff;
+		box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.1);
+		border-radius: 100%;
+	}
+</style>
