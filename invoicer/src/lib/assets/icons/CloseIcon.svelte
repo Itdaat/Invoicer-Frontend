555ba5685fc74948c2bd9ba1ad5bebc057fb0943
Duvie-Gazda <gazda.haimdavid@gmail.com>
2023-01-16 00:52:06 +0200

    added icon

---
+++ invoicer/src/lib/assets/icons/CloseIcon.svelte
@@ -0,0 +1,38 @@
+<svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<rect
+		x="3.57446"
+		y="4.95862"
+		width="1.95735"
+		height="11.766"
+		rx="0.978674"
+		transform="rotate(-45 3.57446 4.95862)"
+		fill="#3D5A80"
+	/>
+	<rect
+		x="3.57446"
+		y="4.95862"
+		width="1.95735"
+		height="11.766"
+		rx="0.978674"
+		transform="rotate(-45 3.57446 4.95862)"
+		fill="#3D5A80"
+	/>
+	<rect
+		x="5.08325"
+		y="13.1901"
+		width="2.00883"
+		height="11.4645"
+		rx="1.00441"
+		transform="rotate(-135 5.08325 13.1901)"
+		fill="#3D5A80"
+	/>
+	<rect
+		x="5.08325"
+		y="13.1901"
+		width="2.00883"
+		height="11.4645"
+		rx="1.00441"
+		transform="rotate(-135 5.08325 13.1901)"
+		fill="#3D5A80"
+	/>
+</svg>
