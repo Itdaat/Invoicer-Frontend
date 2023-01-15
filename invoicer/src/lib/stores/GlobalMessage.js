73d6337a203f757cd05e83f8cc82bba7a8e7cba0
Duvie-Gazda <gazda.haimdavid@gmail.com>
2023-01-16 00:53:00 +0200

    added global message

--- invoicer/src/lib/stores/GlobalMessage.js
+++ invoicer/src/lib/stores/GlobalMessage.js
@@ -1,13 +1,17 @@
 import { writable } from "svelte/store";
 
 /**
- * @type {import("svelte/store").Writable<{
+ * @type {import("svelte/store").Writable<{arr : [{
+ *      id : number
  *      message? : string,
  *      status : "ordinary" | "success" | "error"
  *      func? : Function,
- *      buttonText : string
- * }>}
+ *      timeout : number
+*      buttonText : string
+ * }]
+ * lastId : number}>}
  */
-const GlobalMessageStore = writable({ status: 'ordinary', buttonText: '' });
+// @ts-ignore
+const GlobalMessageStore = writable({ arr: [], lastId: 0 });
 
 export default GlobalMessageStore;
