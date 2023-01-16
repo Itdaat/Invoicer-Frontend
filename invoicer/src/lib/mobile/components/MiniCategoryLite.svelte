2271e809e26a3e3434a06283b50199b120035a9f
Duvie-Gazda <gazda.haimdavid@gmail.com>
2023-01-16 15:44:13 +0200

    added mini category

---
+++ invoicer/src/lib/mobile/components/MiniCategoryLite.svelte
@@ -0,0 +1,38 @@
+<script>
+	export let title = '';
+</script>
+
+<div>
+	<div class="title">{title}</div>
+	<div class="category-container">
+		<slot />
+	</div>
+</div>
+
+<style>
+	.title {
+		font-family: 'Roboto';
+		font-style: normal;
+		font-weight: 500;
+		font-size: 16px;
+		line-height: 19px;
+		/* identical to box height */
+
+		letter-spacing: 1px;
+
+		/* icons */
+
+		color: #3d5a80;
+
+		padding: 0px 20px;
+	}
+	.category-container {
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+		margin-top: 14px;
+		/* overflow-y: scroll; */
+		/* height: 100%; */
+		/* padding-bottom: 40%; */
+	}
+</style>
