c6770dd7a7f8527f4fcf358464db98b0006e323f
Duvie-Gazda <gazda.haimdavid@gmail.com>
2023-01-17 23:15:30 +0200

    added persons page (plug)

---
+++ invoicer/src/routes/mobile/(menu)/(pages)/persons/+page.svelte
@@ -0,0 +1,78 @@
+<script>
+	import { getCmr } from '$lib/api/server/cmr';
+	import { getPersonIds } from '$lib/api/server/persons';
+	import { getTrucks } from '$lib/api/server/transport';
+	import Loader from '$lib/mobile/components/Loader.svelte';
+	import MiniCategoryLite from '$lib/mobile/components/MiniCategoryLite.svelte';
+	import { driverTag } from '../../../../../consts';
+	import { Jumper } from 'svelte-loading-spinners';
+	import { slide } from 'svelte/transition';
+
+	const getCMRSFormatted = async () => {
+		return (await getCmr()).result;
+	};
+</script>
+
+<div class="main">
+	<MiniCategoryLite title="Trucks">
+		{#await getCMRSFormatted()}
+			<div class="loader-container">
+				<Loader status="inProcess" size="60" />
+			</div>
+		{:then cmrs}
+			{#each cmrs as cmr}
+				<div class="cmr" in:slide={{ duration: 700 }}>
+					<div class="license">{cmr.externalNumber}</div>
+					<!-- <div class="brand">{cmr.brandName}</div> -->
+				</div>
+			{/each}
+		{/await}
+	</MiniCategoryLite>
+</div>
+
+<style>
+	.main {
+		padding-top: 27px;
+		padding-bottom: 5px;
+		overflow-y: scroll;
+		user-select: none;
+	}
+
+	.cmr {
+		display: flex;
+		/* grid-template-columns: 1fr 1fr; */
+		justify-content: space-between;
+		align-items: center;
+		width: 80%;
+		border-bottom: 0.2px solid rgba(54, 56, 59, 0.233);
+		padding: 18px 10px;
+	}
+
+	.loader-container {
+		margin-top: 20px;
+	}
+
+	.license {
+		text-align: center;
+		font-family: 'Roboto';
+		font-style: normal;
+		font-weight: 500;
+		font-size: 16px;
+		line-height: 23px;
+		letter-spacing: 2px;
+
+		color: #5e5e75;
+	}
+
+	.brand {
+		text-align: center;
+		font-family: 'Roboto';
+		font-style: normal;
+		font-weight: 400;
+		font-size: 13px;
+		line-height: 15px;
+		letter-spacing: 1px;
+
+		color: #6e6e8b;
+	}
+</style>
