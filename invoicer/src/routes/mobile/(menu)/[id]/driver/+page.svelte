027eed58498b657153b93b1d6b19fd467925b007
Duvie-Gazda <gazda.haimdavid@gmail.com>
2023-01-19 14:54:54 +0200

    added driver page

---
+++ invoicer/src/routes/mobile/(menu)/[id]/driver/+page.svelte
@@ -0,0 +1,105 @@
+<script>
+	import LanguageStore from '$lib/stores/Language';
+	import ShareIcon from '$lib/assets/icons/ShareIcon.svelte';
+	import PrintIcon from '$lib/assets/icons/PrintIcon.svelte';
+	import DeleteIcon from '$lib/assets/icons/DeleteIcon.svelte';
+	import Button from '$lib/templates/Button.svelte';
+	import MiniMenu from '$lib/mobile/templates/MiniMenu.svelte';
+	import { getInvoice } from '$lib/api/server/invoice';
+	import ChangeEntity from '$lib/mobile/components/ChangeEntity.svelte';
+	import { getFullPerson, getPerson } from '$lib/api/server/persons';
+	import { page } from '$app/stores';
+	import MiniCategory from '$lib/mobile/components/MiniCategory.svelte';
+	import { copy } from 'svelte-copy';
+	import { openSuccessMessage } from '$lib/helpers/message';
+	import GlobalMessages from '$lib/mobile/components/GlobalMessages.svelte';
+	import ListItem from '$lib/mobile/components/ListItem.svelte';
+
+	export let title = '';
+	$: t = $LanguageStore;
+
+	const sign = () => {};
+
+	const getPersonFormatted = async () => {
+		const personRes = await getFullPerson($page.params.id);
+		if (personRes.error) {
+			return;
+		}
+		const person = personRes.result;
+		if (!person) {
+			return;
+		}
+		title = person.nickname;
+		return person;
+	};
+</script>
+
+<MiniMenu {title}>
+	<div class="actions" slot="actions">
+		<div class="action-item">
+			<DeleteIcon />
+			<div class="actions-item-title">{t.invoice_delete_title}</div>
+		</div>
+	</div>
+	<div class="main" slot="main">
+		{#await getPersonFormatted() then person}
+			<MiniCategory title={t.person_main}>
+				<ListItem
+					name={t.person_create_first_name}
+					value={person?.firstName}
+					messageText={t.person_copied_first_name}
+				/>
+				<ListItem
+					name={t.person_create_last_name}
+					value={person?.lastName}
+					messageText={t.person_create_last_name}
+				/>
+			</MiniCategory>
+			<MiniCategory title={t.person_create_contact_data}>
+				<ListItem
+					name={t.person_create_email}
+					value={person?.email?.value}
+					messageText={t.person_copied_email}
+					noDataText={t.person_no_data_email}
+				/>
+				<ListItem
+					name={t.person_create_phone}
+					value={person?.phone?.value}
+					messageText={t.person_copied_phone}
+					noDataText={t.person_no_data_phone}
+				/>
+			</MiniCategory>
+		{/await}
+	</div>
+</MiniMenu>
+<ChangeEntity />
+<GlobalMessages />
+
+<style>
+	.actions-item-title {
+		font-family: 'Exo 2';
+		font-style: normal;
+		font-weight: 500;
+		font-size: 17px;
+		line-height: 40px;
+		/* or 235% */
+
+		letter-spacing: 1px;
+
+		color: #204660;
+
+		margin-left: 20px;
+	}
+
+	.action-item {
+		display: flex;
+		align-items: center;
+		width: 90%;
+		margin-left: 20px;
+		margin-top: 3px;
+	}
+
+	.actions {
+		width: 100%;
+	}
+</style>
