af97ab57c9896c8b796b2f88835676d6a9b22963
Duvie-Gazda <gazda.haimdavid@gmail.com>
2023-01-19 23:54:53 +0200

    added input date (init)

---
+++ invoicer/src/lib/templates/InputDate.svelte
@@ -0,0 +1,24 @@
+<script>
+	/** @type {string}*/
+	export let fromDate;
+	/** @type {string}*/
+	export let toDate;
+	/** @type {string}*/
+	export let title;
+</script>
+
+<div class="container">
+	<div class="title">{title}</div>
+	<div class="dates-container">
+		<div class="from">
+			<div class="from-title" />
+			<input type="date" class="input" />
+		</div>
+		<div class="to">
+			<div class="to-title" />
+			<input type="date" class="input" />
+		</div>
+	</div>
+</div>
+
+<style></style>
