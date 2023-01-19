6b57d224549003e4180eab2133d739bd2f3bca49
Duvie-Gazda <gazda.haimdavid@gmail.com>
2023-01-19 23:54:43 +0200

    added filters

---
+++ invoicer/src/lib/mobile/components/filters/PersonFilter.svelte
@@ -0,0 +1,12 @@
+<script>
+	import InputDate from '$lib/templates/InputDate.svelte';
+
+	let fromInput = '';
+	let toInput = '';
+</script>
+
+<div class="main">
+	<InputDate bind:fromDate={fromInput} bind:toDate={toInput} title="Test" />
+</div>
+
+<style></style>
