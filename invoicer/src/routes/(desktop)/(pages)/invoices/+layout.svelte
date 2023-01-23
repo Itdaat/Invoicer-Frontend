995e22ddd930021810deafa82e1af329701b3269
Duvie-Gazda <gazda.haimdavid@gmail.com>
2023-01-23 20:25:06 +0200

    init desktop version

---
+++ invoicer/src/routes/(desktop)/(pages)/invoices/+layout.svelte
@@ -0,0 +1,163 @@
+<script>
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+	import { getCmr, getCmrAllFields } from '$lib/api/server/cmr';
+	import ListContainer from '$lib/desktop/components/ListContainer.svelte';
+	import { openErrorMessage } from '$lib/helpers/message';
+	import CounterBig from '$lib/mobile/components/CounterBig.svelte';
+	import CounterSmall from '$lib/mobile/components/CounterSmall.svelte';
+	import ListItem from '$lib/mobile/components/ListItem.svelte';
+	import Loader from '$lib/mobile/components/Loader.svelte';
+	import MiniCategory from '$lib/mobile/components/MiniCategory.svelte';
+	import FilterStore from '$lib/stores/FilterStore';
+	import LanguageStore from '$lib/stores/Language';
+	import { cmrs } from '../../../../consts';
+	import { fade, slide } from 'svelte/transition';
+
+	$: t = $LanguageStore;
+
+	let data = {
+		sign: '$',
+		cash: 123991,
+		cent: 23
+	};
+
+	let counterSmall = {
+		cash: 12232,
+		cent: 42,
+		sign: '$',
+		title: 'В очікуванні'
+	};
+
+	$: filter = $FilterStore;
+
+	const getCMRSFormatted = async (filters) => {
+		return (await getCmrAllFields(filters)).result;
+	};
+
+	$: cmrsFormatted = getCMRSFormatted(filter);
+
+	const gotoCmr = (/** @type {string | number} */ id) => {
+		goto(cmrs + '/' + id);
+	};
+</script>
+
+<div class="main">
+	<div class="menu">
+		<div class="container">
+			<div class="counter">
+				<CounterBig {...data} />
+				<div class="small-counter-container">
+					<CounterSmall {...counterSmall} />
+					<CounterSmall {...counterSmall} />
+					<CounterSmall {...counterSmall} />
+				</div>
+			</div>
+		</div>
+
+		<div class="lists" transition:fade|local={{ duration: 700 }}>
+			<ListContainer>
+				{#await cmrsFormatted}
+					<div class="loader-container">
+						<Loader status="inProcess" size="60" />
+					</div>
+				{:then cmrs}
+					<MiniCategory title="super">
+						<!-- hello -->
+						{#each cmrs as cmr}
+							<!-- <ListItem name="da" value="lele">Test</ListItem> -->
+							<!-- svelte-ignore a11y-click-events-have-key-events -->
+							<div
+								class="cmr ripple"
+								in:slide|local={{ duration: 700 }}
+								on:click={() => {
+									gotoCmr(cmr.id);
+								}}
+							>
+								<div class="license">{cmr.externalNumber}</div>
+								<!-- <div class="brand">{cmr.brandName}</div> -->
+							</div>
+						{/each}
+					</MiniCategory>
+				{/await}
+			</ListContainer>
+		</div>
+	</div>
+	<div class="data">
+		<slot />
+	</div>
+</div>
+
+<style>
+	.main {
+		display: grid;
+		grid-template-columns: 2fr 4fr;
+		max-height: 100vh;
+	}
+	.lists {
+		width: 100%;
+		height: 95%;
+		display: flex;
+		justify-content: center;
+		margin-top: 20px;
+		padding: 10px 0px;
+	}
+
+	.menu {
+		/* padding-top: 40px; */
+		/* padding-bottom: 25px; */
+		height: 100vh;
+
+		display: flex;
+		justify-content: flex-start;
+		flex-direction: column;
+		align-items: center;
+
+		background: #fbfbfb;
+		box-shadow: inset 0px 0px 15px rgba(0, 0, 0, 0.09);
+
+		font-family: 'Istok Web';
+		font-style: normal;
+		font-weight: 400;
+		font-size: 16px;
+		line-height: 23px;
+		/* identical to box height */
+
+		letter-spacing: 1px;
+
+		color: rgba(61, 90, 128, 0.5);
+	}
+
+	.cmr {
+		display: flex;
+		/* grid-template-columns: 1fr 1fr; */
+		justify-content: space-between;
+		align-items: center;
+		width: 80%;
+		border-bottom: 0.2px solid rgba(54, 56, 59, 0.1);
+		padding: 18px 10px;
+		font-family: 'Roboto';
+		font-style: normal;
+		font-weight: 500;
+		font-size: 20px;
+		line-height: 23px;
+		letter-spacing: 2px;
+
+		color: #5e5e75;
+	}
+
+	.ripple {
+		user-select: none;
+		background-position: center;
+		background-size: 1000%;
+		transition: background 0.8s;
+	}
+	.ripple:hover {
+		background: #ffffff radial-gradient(circle, transparent 1%, #ffffff 1%) center/10000%;
+	}
+	.ripple:active {
+		background-color: #c7c6c6;
+		background-size: 100%;
+		transition: background 0s;
+	}
+</style>
---
+++ invoicer/src/routes/(desktop)/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import CreateEntity from '$lib/desktop/components/CreateEntity.svelte';
+	import Menu from '$lib/desktop/components/Menu.svelte';
+</script>
+
+<div class="main">
+	<Menu />
+	<div class="list-container"><slot /></div>
+	<!-- <div class="data"><slot /></div> -->
+</div>
+<CreateEntity />
+
+<style>
+	.main {
+		display: grid;
+		grid-template-columns: minmax(10%, 250px) 5fr;
+		height: 100vh;
+	}
+</style>
