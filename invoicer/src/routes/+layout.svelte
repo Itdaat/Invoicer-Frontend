d73b9680b23fe66c676c1b7ca8e31b11271a6bb6
Duvie-Gazda <gazda.haimdavid@gmail.com>
2023-01-26 15:32:01 +0200

    added  checking firm

--- invoicer/src/routes/+layout.svelte
+++ invoicer/src/routes/+layout.svelte
@@ -1,41 +1,11 @@
 <script>
-	import { getCurrentFirm, setCurrentFirm } from '$lib/api/server/firm';
+	import { goto } from '$app/navigation';
+	import { getFirm, setCurrentFirm } from '$lib/api/server/firm';
+	import { getCookie, setCookie } from '$lib/helpers/cookies';
 	import LanguageStore from '$lib/stores/Language';
 
-	/** @type {import('./$types').PageData} */
 	export let data;
-
-	localStorage.setItem('firm', JSON.stringify(data.firm));
-	setCurrentFirm();
 	LanguageStore.set(JSON.parse(data.t));
 </script>
 
 <slot />
-
-<!-- <style>
-	:global(.ripple-light) {
-		background-position: center;
-		transition: background 0.8s;
-	}
-	:global(.ripple-light:hover) {
-		background: #ffffff radial-gradient(circle, transparent 1%, #ffffff 1%) center/15000%;
-	}
-	:global(.ripple-light:active) {
-		background-color: #c7c6c6;
-		background-size: 100%;
-		transition: background 0s;
-	}
-
-	:global(.ripple-dark) {
-		background-position: center;
-		transition: background 0.8s;
-	}
-	:global(.ripple-dark:hover) {
-		background: #3d5a80 radial-gradient(circle, transparent 1%, #3d5a80 1%) center/18000%;
-	}
-	:global(.ripple-dark:active) {
-		background-color: #6280a7;
-		background-size: 100%;
-		transition: background 0s;
-	}
-</style> -->
