a8843f74ad6a40edabd4ab1bf93a7c45a20ca2c6
Duvie-Gazda <gazda.haimdavid@gmail.com>
2023-01-26 14:40:36 +0200

    added checking firm for mobile

--- invoicer/src/routes/mobile/(menu)/+layout.svelte
+++ invoicer/src/routes/mobile/(menu)/+layout.svelte
@@ -1,1 +1,46 @@
-<slot />
+<script>
+	import { goto } from '$app/navigation';
+	import { getCurrentFirm, getFirm } from '$lib/api/server/firm';
+	import CreateFirm from '$lib/desktop/components/firm/CreateFirm.svelte';
+	import { getCookie, setCookie } from '$lib/helpers/cookies';
+	import LanguageStore from '$lib/stores/Language';
+	import { beforeUpdate, onMount } from 'svelte';
+	$: t = $LanguageStore;
+
+	// const firm = JSON.parse(localStorage.getItem('firm') || '{}');
+
+	// const firmCheck = async () => {
+	// 	if (!firm || firm.error) {
+	// 		localStorage.setItem('create_title', t.firm_create);
+	// 		goto('/mobile/create/firm');
+	// 	}
+	// };
+
+	// onMount(() => firmCheck());
+
+	const GetFirm = async () => {
+		let firm;
+		const firmId = getCookie('firmId');
+		const firmRes = await getFirm(firmId);
+		if (firmRes.error) {
+			localStorage.setItem('create_title', t.firm_create);
+			goto('/mobile/create/firm');
+		}
+		if (firmRes.result.length > 0) {
+			// @ts-ignore
+			firm = firmRes.result[0];
+		} else {
+			firm = firmRes.result;
+		}
+		setCookie('firmId', firm.id);
+		localStorage.setItem('firm', JSON.stringify(firm));
+	};
+
+	// onMount(GetFirm);
+	// GetFirm();
+	// beforeUpdate(GetFirm);
+</script>
+
+{#await GetFirm() then Get}
+	<slot />
+{/await}
