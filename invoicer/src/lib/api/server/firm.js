e7a65b42896266992d06160a7af42124a6c68a74
Duvie-Gazda <gazda.haimdavid@gmail.com>
2023-01-26 14:38:41 +0200

    added getting firm and creating firm api

--- invoicer/src/lib/api/server/firm.js
+++ invoicer/src/lib/api/server/firm.js
@@ -1,53 +1,115 @@
+import { postAuthRequestJson } from '$lib/helpers/apiHelper';
 import { getCookie, setCookie } from '$lib/helpers/cookies';
+import { emailDataType, legalAddressType, paymentIban, phoneDataType, postalAddressType } from '../../../consts';
 
 /**
  * @export
  * @returns {Promise<import('../../../types/Entities').Response<import('../../../types/Entities').Firm[]>>}
  */
 export async function getFirms() {
-    // const token = getToken() || '';
-    return {
-        error: null,
-        result: [
-            {
-                id: '1',
-                name: 'Super',
-                messageCount: 0
-            },
-            {
-                id: '2',
-                name: 'Koala',
-                messageCount: 2
-            }
-        ]
-    }
+    const firms = await postAuthRequestJson('firm/get', {});
+    return firms;
 }
 /**
- * @param {string | undefined} firmId
+ * @param {string | null} firmId
  * @export
  * @returns {Promise<import('../../../types/Entities').Response<import('../../../types/Entities').Firm>>}
  */
-export async function getFirm(firmId) {
-    const firms = await getFirms();
-    const firm = firms.result.filter(el => el.id == firmId);
-    return {
-        error: null,
-        result: firm[0]
+export async function getFirm(firmId = null) {
+    const firm = await postAuthRequestJson('firm/get', { firmId });
+    return firm;
+}
+
+// create firm  
+/**
+ * 
+ * 
+ * @export
+ * @param {string} name 
+ * @param {string} taxNumber 
+ * @param {string} email 
+ * @param {string} phone 
+ * @param {import('../../../types/Entities').Address} legalAddress 
+ * @param {import('../../../types/Entities').Address} postalAddress 
+ * @param {string} ibanCode 
+ * @returns 
+ */
+export async function createFirm(name, taxNumber, email, phone, legalAddress, postalAddress, ibanCode) {
+    const mainReqBody = {
+        name, taxNumber
     }
+    const firm = await postAuthRequestJson('firm/create', mainReqBody);
+    if (firm.error) {
+        return firm;
+    }
+
+    const firmId = firm.result.id;
+
+    const emailReqBody = {
+        typeId: emailDataType,
+        value: email,
+        firmId: firmId
+    }
+    const emailRes = await postAuthRequestJson('firm/contact-data/create', emailReqBody);
+    if (emailRes.error) {
+        return emailRes;
+    }
+
+    const phoneReqBody = {
+        typeId: phoneDataType,
+        value: phone,
+        firmId: firmId
+    }
+    const phoneRes = await postAuthRequestJson('firm/contact-data/create', phoneReqBody);
+    if (phoneRes.error) {
+        return phoneRes;
+    }
+
+    const ibanReqBody = {
+        typeId: paymentIban,
+        value: ibanCode,
+        firmId: firmId
+    }
+    const ibanRes = await postAuthRequestJson('firm/payment-data/create', ibanReqBody);
+    if (ibanRes.error) {
+        return ibanRes;
+    }
+
+    const legalReqBody = {
+        ...legalAddress,
+        typeId: legalAddressType,
+        firmId: firmId
+    }
+    const legalRes = await postAuthRequestJson('firm/address/create', legalReqBody);
+    if (legalRes.error) {
+        return legalRes;
+    }
+
+    const postalReqBody = {
+        ...postalAddress,
+        typeId: postalAddressType,
+        firmId: firmId
+    }
+    const postalRes = await postAuthRequestJson('firm/address/create', postalReqBody);
+    if (postalRes.error) {
+        return postalRes;
+    }
+
+    return firm;
 }
 
 export function getCurrentFirm() {
     return JSON.parse(getCookie('firmId') || '{}');
 }
 
-export async function setCurrentFirm() {
-    const currentFirm = getCookie('firmId');
-    if (currentFirm) {
-        return currentFirm;
-    }
-    const firms = await getFirms();
-    setCookie('firmId', firms.result[0].id);
-    return firms.result[0]
+export async function setCurrentFirm(firmId = null) {
+    // const currentFirm = getCookie('firmId');
+    // if (currentFirm) {
+    //     return currentFirm;
+    // }
+    const firm = await getFirm(firmId);
+    setCookie('firmId', firm.result.id);
+    return firm.result.id;
 }
 
 /**
@@ -63,7 +125,6 @@ export async function changeFirm(firmId) {
         return false;
     }
     setCookie('firmId', firm.result.id.toString());
-    // setCookie('firmId', "2");
     localStorage.setItem('firm', JSON.stringify(firm.result));
     return true;
 }
