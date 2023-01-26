efb220cce436cff6b841ec8eebcd52d64e274247
Duvie-Gazda <gazda.haimdavid@gmail.com>
2023-01-26 14:40:16 +0200

    init firm checking for desktop

--- invoicer/src/routes/(desktop)/+layout.svelte
+++ invoicer/src/routes/(desktop)/+layout.svelte
@@ -1,8 +1,18 @@
 <script>
+	import { getCurrentFirm } from '$lib/api/server/firm';
 	import CreateEntity from '$lib/desktop/components/CreateEntity.svelte';
+	import CreateFirm from '$lib/desktop/components/firm/CreateFirm.svelte';
 	import Menu from '$lib/desktop/components/Menu.svelte';
+
+	const firmId = getCurrentFirm();
+	const firm = JSON.parse(localStorage.getItem('firm') || '{}');
+
+	let showCreateFirm = !firm || firm.error;
 </script>
 
+{#if showCreateFirm}
+	<CreateFirm showFirm={showCreateFirm} />
+{/if}
 <div class="main">
 	<Menu />
 	<div class="list-container"><slot /></div>
