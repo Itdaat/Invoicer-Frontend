04d802b62883375262393701468a03c932b0e231
Duvie-Gazda <gazda.haimdavid@gmail.com>
2023-01-27 01:03:23 +0200

    updated order api

--- invoicer/src/lib/api/server/order.js
+++ invoicer/src/lib/api/server/order.js
@@ -1,13 +1,50 @@
 import { postAuthRequestJson } from '$lib/helpers/apiHelper';
 
-export async function getOrders(orderId, firmId, externalOrderNumber, orderNumber = null) {
-    const reqBody = { orderId, firmId, externalOrderNumber, orderNumber };
-    const result = await postAuthRequestJson('/order/get', reqBody);
+/**
+ * 
+ * 
+ * @export
+ * @param {any} firmId 
+ * @param {any} filters 
+ * @returns {Promise<import('../../../types/Entities').Response<import('../../../types/Entities').Order[]>>}
+ */
+export async function getOrders(firmId, filters) {
+    const reqBody = { firmId, ...filters };
+    const result = await postAuthRequestJson('order/get', reqBody);
     return result;
 }
 
-export async function updateOrder(orderId, firmId, externalOrderNumber, orderNumber = null) {
-    const reqBody = { orderId, firmId, externalOrderNumber, orderNumber };
-    const result = await postAuthRequestJson('/order/update', reqBody);
-    return result;
+/**
+ * 
+ * 
+ * @export
+ * @param {any} firmId 
+ * @param {any} orderId 
+ * @returns {Promise<import('../../../types/Entities').Order>}
+ */
+export async function getOrder(firmId, orderId) {
+    const reqBody = { firmId, orderId };
+    const result = await postAuthRequestJson('order/get', reqBody);
+    return result.result[0];
 }
+
+/**
+ * 
+ * 
+ * @export
+ * @param {any} orderId 
+ * @param {any} firmId 
+ * @param {import('../../../types/Entities').Transport} truck 
+ * @param {import('../../../types/Entities').Transport} trailer 
+ * @param {import('../../../types/Entities').Person} driver 
+ * @param {any} [orderNumber=null] 
+ * @returns 
+ */
+export async function updateOrder(orderId, firmId, truck, trailer, driver, orderNumber = null) {
+    // const reqBody = { orderId, firmId, orderNumber };
+    // const result = await postAuthRequestJson('/order/update', reqBody);
+    // return result;
+
+
+}
+
