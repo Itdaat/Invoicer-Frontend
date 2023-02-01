9d9c538e4c6df3e94494d67f1a1e365656c39f65
Duvie-Gazda <gazda.haimdavid@gmail.com>
2023-02-01 02:17:40 +0200

    updated transport api

--- invoicer/src/lib/api/server/transport.js
+++ invoicer/src/lib/api/server/transport.js
@@ -1,5 +1,5 @@
 import { postAuthRequestJson } from '$lib/helpers/apiHelper';
-import { TRANSPORT_TRAILER, TRANSPORT_TRUCK } from '../../../consts';
+import { trailers, TRANSPORT_TRAILER, TRANSPORT_TRUCK, trucks } from '../../../consts';
 
 /**
  * 
@@ -19,12 +19,12 @@ export async function createTrailer(licenseNumber, transportBrand) {
  * 
  * @export
  * @param {string} licenseNumber 
- * @param {string} transportBrand 
+ * @param {string} transportBrandName 
  * @returns 
  */
-export async function createTruck(licenseNumber, transportBrand) {
+export async function createTruck(licenseNumber, transportBrandName) {
     const reqBody = {
-        licenseNumber, transportBrand, transportTypeId: TRANSPORT_TRUCK
+        licenseNumber, transportBrandName, transportTypeId: TRANSPORT_TRUCK
     };
     const result = await postAuthRequestJson('transport/create', reqBody);
     return result;
@@ -36,12 +36,12 @@ export async function createTruck(licenseNumber, transportBrand) {
  * @param {string | null} [id=null] 
  * @param {string | null} [name=null] 
  * @param {string | null} [licenseNumber=null] 
- * @param {string | null} [transportBrand=null] 
+ * @param {string | null} [transportBrandName=null] 
  * @returns {Promise<import('../../../types/Entities').Response<import('../../../types/Entities').Transport[]>>}
  */
-export async function getTrucks(id = null, name = null, licenseNumber = null, transportBrand = null) {
+export async function getTrucks(id = null, name = null, licenseNumber = null, transportBrandName = null) {
     const reqBody = {
-        id, name, licenseNumber, transportBrand, transportTypeId: TRANSPORT_TRUCK
+        id, name, licenseNumber, transportBrandName, transportTypeId: TRANSPORT_TRUCK
     };
     const result = await postAuthRequestJson('transport/get', reqBody);
     return result;
@@ -54,12 +54,12 @@ export async function getTrucks(id = null, name = null, licenseNumber = null, tr
  * @param {string | null} [id=null] 
  * @param {string | null} [name=null] 
  * @param {string | null} [licenseNumber=null] 
- * @param {string | null} [transportBrand=null] 
+ * @param {string | null} [transportBrandName=null] 
  * @returns {Promise<import('../../../types/Entities').Response<import('../../../types/Entities').Transport[]>>}
  */
-export async function getTrailers(id = null, name = null, licenseNumber = null, transportBrand = null) {
+export async function getTrailers(id = null, name = null, licenseNumber = null, transportBrandName = null) {
     const reqBody = {
-        id, name, licenseNumber, transportBrand, transportTypeId: TRANSPORT_TRAILER
+        id, name, licenseNumber, transportBrandName, transportTypeId: TRANSPORT_TRAILER
     };
     const result = await postAuthRequestJson('transport/get', reqBody);
     return result;
@@ -121,4 +121,62 @@ export async function updateTrailer(id, transportBrandName) {
     };
     const result = await postAuthRequestJson('transport/update', reqBody);
     return result;
+}
+
+export async function getTrucksAutocomplete(truckLicense) {
+    let trucks = await getTrucks(null, null, truckLicense, null);
+
+    if (truckLicense) {
+        let trucksBrand = await getTrucks(null, null, null, truckLicense);
+        trucks.result = [...trucks.result, ...trucksBrand.result];
+    }
+
+    if (!trucks.result) {
+        return;
+    }
+    /**
+     * @type {{ value: string; name: string;}[]}
+     */
+    let trucksRes = [];
+    trucks.result.forEach((truckRes) => {
+        let brandName = '';
+        if (truckRes.brandName) {
+            brandName = ' (' + truckRes.brandName + ')';
+        }
+        const newTruck = truckRes.licenseNumber + brandName;
+        trucksRes.push({
+            value: truckRes.licenseNumber,
+            name: newTruck
+        });
+    });
+    return trucksRes;
+}
+
+export async function getTrailersAutocomplete(trailerLicense) {
+    let trailers = await getTrailers(null, null, trailerLicense);
+
+    if (trailerLicense) {
+        let trailerBrand = await getTrailers(null, null, null, trailerLicense);
+        trailers.result = [...trailers.result, ...trailerBrand.result];
+    }
+
+    if (!trailers.result) {
+        return;
+    }
+    /**
+     * @type {{ value: string; name: string;}[]}
+     */
+    let trailersRes = [];
+    trailers.result.forEach((trailerRes) => {
+        let brandName = '';
+        if (trailerRes.brandName) {
+            brandName = ' (' + trailerRes.brandName + ')';
+        }
+        const newTrailer = trailerRes.licenseNumber + brandName;
+        trailersRes.push({
+            value: trailerRes.licenseNumber,
+            name: newTrailer
+        })
+    });
+    return trailersRes;
 }
