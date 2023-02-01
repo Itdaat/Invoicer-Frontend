596e51501c7823c72002d5d3828888ef9db2d1b0
Duvie-Gazda <gazda.haimdavid@gmail.com>
2023-02-01 17:01:03 +0200

    init trucks desktop

---
+++ invoicer/src/routes/(desktop)/(pages)/trucks/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import PageTemplate from '$lib/desktop/templates/PageTemplate.svelte';
+</script>
+
+<PageTemplate>
+	<div slot="left">Hello</div>
+
+	<div slot="main"><slot /></div>
+</PageTemplate>
