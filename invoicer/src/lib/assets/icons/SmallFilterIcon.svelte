846d542bdf6314736af731eaec82bf9ebbe85114
Duvie-Gazda <gazda.haimdavid@gmail.com>
2023-02-01 16:56:41 +0200

    added small filter icon

---
+++ invoicer/src/lib/assets/icons/SmallFilterIcon.svelte
@@ -0,0 +1,8 @@
+<svg width="20" height="17" viewBox="0 0 20 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<rect y="1.42859" width="20" height="2.14286" rx="1.07143" fill="#6986AC" fill-opacity="0.7" />
+	<rect x="12.8571" width="2.85714" height="5" rx="1.42857" fill="#6986AC" fill-opacity="0.7" />
+	<rect x="10" y="11.4286" width="2.85714" height="5" rx="1.42857" fill="#6986AC" fill-opacity="0.7" />
+	<rect y="12.8571" width="20" height="2.14286" rx="1.07143" fill="#6986AC" fill-opacity="0.7" />
+	<rect x="3.57143" y="5.71429" width="2.85714" height="5" rx="1.42857" fill="#6986AC" fill-opacity="0.7" />
+	<rect y="7.14288" width="20" height="2.14286" rx="1.07143" fill="#6986AC" fill-opacity="0.7" />
+</svg>
