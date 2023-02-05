4b64a1b1a3c1fb3b15d0532fef8d37ebd87f391a
Duvie-Gazda <gazda.haimdavid@gmail.com>
2023-02-05 18:36:10 +0200

    updated main page template

--- invoicer/src/lib/desktop/templates/MainPageTemplate.svelte
+++ invoicer/src/lib/desktop/templates/MainPageTemplate.svelte
@@ -15,24 +15,18 @@
 		font-weight: 400;
 		font-size: 16px;
 		line-height: 23px;
-		/* identical to box height */
-
-		display: flex;
-		align-items: center;
 		letter-spacing: 1px;
 		color: rgba(61, 90, 128, 0.5);
 
-		display: flex;
-		justify-content: center;
-		align-items: center;
+		width: 80%;
 	}
 
 	.container {
 		display: flex;
 		justify-content: center;
-		align-items: center;
+		/* align-items: center; */
 
-		width: 100%;
+		/* width: 100%; */
 		height: 100%;
 	}
 </style>
