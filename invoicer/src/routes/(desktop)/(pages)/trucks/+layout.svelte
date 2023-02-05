a5fb80dacb9a7f2d93d68c3093862ede85765345
Duvie-Gazda <gazda.haimdavid@gmail.com>
2023-02-05 19:15:06 +0200

    added trucks

--- invoicer/src/routes/(desktop)/(pages)/trucks/+layout.svelte
+++ invoicer/src/routes/(desktop)/(pages)/trucks/+layout.svelte
@@ -2,17 +2,19 @@
 	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
 	import { getTrailersAllFields, getTrucksAllFields } from '$lib/api/server/transport';
+	import FilterPopup from '$lib/desktop/components/FilterPopup.svelte';
 	import ListContainer from '$lib/desktop/components/ListContainer.svelte';
 	import MiniCategory from '$lib/desktop/components/MiniCategory.svelte';
 	import MainPageTemplate from '$lib/desktop/templates/MainPageTemplate.svelte';
 	import PageTemplate from '$lib/desktop/templates/PageTemplate.svelte';
-	import FilterPopup from '$lib/mobile/components/FilterPopup.svelte';
 	import TrailerFilter from '$lib/mobile/components/filters/TrailerFilter.svelte';
+	import TruckFilter from '$lib/mobile/components/filters/TruckFilter.svelte';
 	import Loader from '$lib/mobile/components/Loader.svelte';
 	import FilterStore from '$lib/stores/FilterStore';
 	import LanguageStore from '$lib/stores/Language';
+	import { quartInOut } from 'svelte/easing';
 	import { slide } from 'svelte/transition';
-	import { trailers, trucks } from '../../../../consts';
+	import { trucks } from '../../../../consts';
 
 	$: filter = $FilterStore;
 	$: t = $LanguageStore;
@@ -25,11 +27,15 @@
 	 * @param {{ brand?: any; licenseNumber?: any; }} filters
 	 */
 	const getTrucksFormatted = async (filters) => {
-		return (await getTrucksAllFields({ ...filters })).result;
+		return (await getTrucksAllFields(filters)).result;
 	};
 
 	$: trucksApi = getTrucksFormatted(filter);
-	const gotoTruck = (id) => {
+	$: if ($page.url.pathname == trucks) {
+		trucksApi = getTrucksFormatted(filter);
+	}
+	/** @param {string} id*/
+	const gotoTrucks = (id) => {
 		goto(trucks + '/' + id);
 	};
 </script>
@@ -39,28 +45,30 @@
 		<MiniCategory title={t.trailers_many} filter bind:showFilter>
 			{#await trucksApi}
 				<Loader status="inProcess" />
-			{:then trucks}
-				{#each trucks as trailer}
-					<div class="trailer-container" class:active={id == trailer.id} in:slide={{ delay: 400 }} out:slide={{ duration: 300 }}>
-						<!-- svelte-ignore a11y-click-events-have-key-events -->
-						<div
-							class="trailer"
-							on:click={() => {
-								gotoTruck(trailer.id);
-							}}
-						>
-							<div class="license">{trailer.licenseNumber}</div>
-							{#if trailer.brandName}
-								<div class="brand">{trailer.brandName}</div>
-							{/if}
+			{:then trailers}
+				<div class="main" in:slide={{ delay: 400 }} out:slide={{ duration: 300 }}>
+					{#each trailers as trailer}
+						<div class="trailer-container" class:active={id == trailer.id}>
+							<!-- svelte-ignore a11y-click-events-have-key-events -->
+							<div
+								class="trailer"
+								on:click={() => {
+									gotoTrucks(trailer.id);
+								}}
+							>
+								<div class="license">{trailer.licenseNumber}</div>
+								{#if trailer.brandName}
+									<div class="brand">{trailer.brandName}</div>
+								{/if}
+							</div>
 						</div>
-					</div>
-				{/each}
+					{/each}
+				</div>
 			{/await}
 		</MiniCategory>
 		{#if showFilter}
 			<FilterPopup bind:showFilter>
-				<TrailerFilter />
+				<TruckFilter />
 			</FilterPopup>
 		{/if}
 	</ListContainer>
@@ -79,24 +87,32 @@
 		padding: 18px 10px;
 	}
 
+	.main {
+		width: 100%;
+	}
+
 	/* .trailer:last-of-type {
 		border-top: 0.2px solid rgba(54, 56, 59, 0.233);
 		border-bottom: 0.2px solid rgba(54, 56, 59, 0.233);
 	} */
 
 	.trailer-container {
-		width: 100%;
 		display: flex;
 		justify-content: center;
 		transition: all 5s;
 		transition: background 1s;
 		user-select: none;
 		cursor: pointer;
+		padding: 0px 5px 0px 12px;
 	}
 
 	.active {
+		width: 100%;
 		background: rgba(61, 90, 128, 0.1);
-		padding-right: 30px;
+		/* padding-right: 20px; */
+		/* padding-left: 300px; */
+		margin-left: -17px;
+		/* padding-right: 10px; */
 		color: #3f3f57;
 	}
 </style>
--- invoicer/src/routes/(desktop)/(pages)/trucks/+page.svelte
+++ invoicer/src/routes/(desktop)/(pages)/trucks/+page.svelte
@@ -1,1 +1,11 @@
-hello
+<script>
+	import MainPageTemplate from '$lib/desktop/templates/MainPageTemplate.svelte';
+	import SmallMainPageTemplate from '$lib/desktop/templates/SmallMainPageTemplate.svelte';
+	import LanguageStore from '$lib/stores/Language';
+
+	$: t = $LanguageStore;
+</script>
+
+<SmallMainPageTemplate>{t.no_selected_truck}</SmallMainPageTemplate>
+
+<style></style>
