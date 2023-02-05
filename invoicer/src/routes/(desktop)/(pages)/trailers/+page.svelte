e3496576142bc6a3347b6832755d4ab5af512832
Duvie-Gazda <gazda.haimdavid@gmail.com>
2023-02-05 18:42:42 +0200

    added trailer page

---
+++ invoicer/src/routes/(desktop)/(pages)/trailers/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import MainPageTemplate from '$lib/desktop/templates/MainPageTemplate.svelte';
+	import SmallMainPageTemplate from '$lib/desktop/templates/SmallMainPageTemplate.svelte';
+	import LanguageStore from '$lib/stores/Language';
+
+	$: t = $LanguageStore;
+</script>
+
+<SmallMainPageTemplate>{t.no_selected_trailer}</SmallMainPageTemplate>
+
+<style></style>
