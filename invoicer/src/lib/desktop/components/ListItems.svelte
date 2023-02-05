46fc1963c0b85cdfb894734bd982396ea40ebe6c
Duvie-Gazda <gazda.haimdavid@gmail.com>
2023-02-05 19:54:53 +0200

    added list items

---
+++ invoicer/src/lib/desktop/components/ListItems.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { slide } from 'svelte/transition';
+
+	/** @type {boolean} */
+	export let animate;
+</script>
+
+{#if animate}
+	<div class="main" in:slide={{ delay: 400 }} out:slide={{ duration: 300 }}>
+		<slot />
+	</div>
+{:else}
+	<div class="main">
+		<slot />
+	</div>
+{/if}
+
+<style>
+	.main {
+		width: 100%;
+	}
+</style>
