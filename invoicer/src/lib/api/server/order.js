35d68367b64e9fc44d6131c047792475d93165de
Duvie-Gazda <gazda.haimdavid@gmail.com>
2023-02-07 17:51:43 +0200

    added order autocomplete

--- invoicer/src/lib/api/server/order.js
+++ invoicer/src/lib/api/server/order.js
@@ -10,6 +10,7 @@ import { postAuthRequestJson } from '$lib/helpers/apiHelper';
  */
 export async function getOrders(firmId, filters) {
     const reqBody = { firmId, ...filters };
+    console.log('puk')
     const result = await postAuthRequestJson('order/get', reqBody);
     return result;
 }
@@ -48,6 +49,36 @@ export async function updateOrder(orderId, firmId, truck, trailer, personId, ord
     const reqBody = { orderId, firmId, truck, trailer, personId };
     const result = await postAuthRequestJson('order/update', reqBody);
     return result;
+}
 
+export async function getOrderAutoComplete(firmId, orderNumber) {
+    const reqBody = { name: orderNumber, firmId: firmId };
+    const orders = await postAuthRequestJson('order/get/autocomplete', reqBody)
+    // let orders = await getOrders(firmId, { orderNumber });
+
+    // const ordersExternal = await getOrder(firmId, { externalOrderNumber: orderNumber });
+    // const ordersTrailer = await getOrder
+
+    if (!orders.result || orders.result.length <= 0) {
+        return;
+    }
+
+    /**
+     * @type {{ value: any; name: string; }[]}
+     */
+    let ordersRes = [];
+    orders.result.forEach(order => {
+        // order.orderNumber;
+        let res = '';
+        res += ' (' + order.truck.licenseNumber + ") ";
+        res = order.orderNumber + res;
+        ordersRes.push({
+            value: order.orderNumber,
+            pseudoValue: order.id,
+            name: res
+        })
+    })
+
+    return ordersRes;
 
 }
---
+++ invoicer/src/routes/mobile/(menu)/create/invoice/+page.svelte
@@ -0,0 +1,245 @@
+<script>
+	import { getCmr } from '$lib/api/server/cmr';
+	import { getCurrentFirmId } from '$lib/api/server/firm';
+	import { getOrder, getOrderAutoComplete } from '$lib/api/server/order';
+	import { getTrailersAutocomplete, getTrucksAutocomplete } from '$lib/api/server/transport';
+	import CreateProduct from '$lib/desktop/components/CreateProduct.svelte';
+	import { dateToString } from '$lib/helpers/date';
+	import MiniCategory from '$lib/mobile/components/MiniCategory.svelte';
+	import SaveEntity from '$lib/mobile/components/SaveEntity.svelte';
+	import LanguageStore from '$lib/stores/Language';
+	import LabeledInput from '$lib/templates/LabeledInput.svelte';
+	import { slide } from 'svelte/transition';
+
+	$: t = $LanguageStore;
+
+	const firmId = getCurrentFirmId();
+	let lastProductId = 0;
+
+	/** @type {import('src/types/Response').ResponseStatus}*/
+	let responseStatus = 'none';
+
+	/** @type {string} */
+	let date = dateToString(new Date());
+	// let date;
+	/** @type {string} */
+	let number;
+	/** @type {string} */
+	let downloadAddress;
+	/** @type {string} */
+	let unloadAddress;
+	/** @type {number} */
+	let sum;
+	/** @type {Array<{id:number,name: string, price : string}>} */
+	let products = [];
+	/** @type {Array<number>} */
+	let orders;
+	/** @type {string} */
+	let orderNumber;
+	/** @type {string} */
+	let truck;
+	/** @type {string} */
+	let trailer;
+
+	/** @type {'ordinary' | 'success' | 'error'}*/
+	let dateStatus;
+	/** @type {'ordinary' | 'success' | 'error'}*/
+	let numberStatus;
+	/** @type {'ordinary' | 'success' | 'error'}*/
+	let downloadAddressStatus;
+	/** @type {'ordinary' | 'success' | 'error'}*/
+	let unloadAddressStatus;
+	/** @type {'ordinary' | 'success' | 'error'}*/
+	let sumStatus;
+	/** @type {Array<'ordinary' | 'success' | 'error'>}*/
+	let productsStatus = [];
+	/** @type {Array<'ordinary' | 'success' | 'error'>}*/
+	let ordersStatus = [];
+	/** @type {'ordinary' | 'success' | 'error'}*/
+	let orderNumberStatus;
+	/** @type {'ordinary' | 'success' | 'error'}*/
+	let truckStatus = 'ordinary';
+	/** @type {'ordinary' | 'success' | 'error'}*/
+	let trailerStatus = 'ordinary';
+
+	let productName;
+	let productValue;
+
+	/** @type {string}*/
+	let orderId;
+
+	/** @param {string} id */
+	const orderChanged = async (id) => {
+		orderId = '';
+		if (id === null || id === undefined || id === '') return;
+
+		const order = await getOrder(firmId, id);
+		unloadAddress = order.unloadAddress;
+		downloadAddress = order.downloadAddress;
+		truck = order.truck.licenseNumber;
+		trailer = order.trailer.licenseNumber;
+		sum = order.price;
+
+		for (let product of order.products) {
+			createProduct(product.name, product.price);
+		}
+	};
+
+	const save = () => {
+		responseStatus = 'inProcess';
+	};
+
+	/**
+	 * @param {string} name
+	 * @param {string} value
+	 */
+	const createProduct = (name, value) => {
+		console.log(name, value);
+		products = [
+			...products,
+			{
+				id: lastProductId++,
+				name,
+				price: value
+			}
+		];
+	};
+
+	/**
+	 * @param {number} id
+	 */
+	const deleteProduct = (id) => {
+		const newProductsList = products.filter((product) => product.id != id);
+		products = newProductsList;
+	};
+
+	$: trucksSuggestions = getTrucksAutocomplete(truck);
+	$: trailerSuggestions = getTrailersAutocomplete(trailer);
+	$: orderSuggestions = getOrderAutoComplete(firmId, orderNumber);
+
+	$: orderChanged(orderId);
+
+	$: productsList = products;
+	// $: products = createProduct(productName, productValue);
+</script>
+
+<div class="main">
+	<MiniCategory title={t.invoice_main}>
+		<LabeledInput
+			bind:value={number}
+			status={numberStatus}
+			label={t.invoice_number}
+			placeHolder={t.invoice_number}
+			message={t.invoice_number_empty}
+		/>
+		<LabeledInput
+			bind:value={date}
+			status={dateStatus}
+			label="{t.invoice_date}pseudoValue"
+			placeHolder={t.invoice_date}
+			message={t.invoice_date_empty}
+			type="date"
+		/>
+	</MiniCategory>
+	<MiniCategory title={t.invoice_order}>
+		<LabeledInput
+			bind:value={orderNumber}
+			status={orderNumberStatus}
+			label={t.invoice_order_number}
+			placeHolder={t.invoice_order_number}
+			message={t.invoice_order_number_empty}
+			autocomplete
+			bind:pseudoValue={orderId}
+			suggestionsApi={orderSuggestions}
+		/>
+		<LabeledInput
+			bind:value={downloadAddress}
+			status={downloadAddressStatus}
+			label={t.invoice_download_address}
+			placeHolder={t.invoice_download_address}
+			message={t.invoice_download_address_empty}
+		/>
+		<LabeledInput
+			bind:value={unloadAddress}
+			status={unloadAddressStatus}
+			label={t.invoice_unload_address}
+			placeHolder={t.invoice_unload_address}
+			message={t.invoice_unload_address_empty}
+		/>
+	</MiniCategory>
+	<MiniCategory title={t.transport_title}>
+		<LabeledInput
+			bind:value={truck}
+			status={truckStatus}
+			label={t.order_truck}
+			placeHolder={t.order_truck}
+			message={t.order_truck_empty}
+			autocomplete
+			suggestionsApi={trucksSuggestions}
+		/>
+		<LabeledInput
+			bind:value={trailer}
+			status={trailerStatus}
+			label={t.order_trailer}
+			placeHolder={t.order_trailer}
+			message={t.order_truck_empty}
+			autocomplete
+			suggestionsApi={trailerSuggestions}
+		/>
+	</MiniCategory>
+	<MiniCategory title={t.invoice_products}>
+		{#if productsList?.length > 0}
+			{#each productsList as product}
+				<div class="product" transition:slide>
+					<LabeledInput bind:value={product.price} label={product.name} />
+					<!-- svelte-ignore a11y-click-events-have-key-events -->
+					<div
+						class="delete"
+						on:click={() => {
+							deleteProduct(product.id);
+						}}
+					>
+						{t.invoice_product_delete}
+					</div>
+				</div>
+			{/each}
+		{/if}
+		<CreateProduct
+			bind:name={productName}
+			bind:value={productValue}
+			onButtonClick={() => {
+				createProduct(productName, productValue);
+			}}
+			valueType={'number'}
+			namePlaceHolder={t.invoice_new_product_name}
+			valuePlaceHolder={t.invoice_new_product_value}
+		/>
+	</MiniCategory>
+	<MiniCategory title={t.invoice_conclusion}>
+		<LabeledInput bind:value={sum} status={sumStatus} label={t.invoice_sum} placeHolder={t.invoice_sum} message={t.invoice_sum_empty} type="number" />
+	</MiniCategory>
+	<SaveEntity {save} status={responseStatus} />
+</div>
+
+<style>
+	.product {
+		display: grid;
+		align-items: center;
+		justify-content: space-between;
+		gap: 10px;
+		grid-template-columns: 6fr 1fr;
+		width: 85%;
+	}
+	.delete {
+		font-family: 'Exo 2';
+		font-style: normal;
+		font-weight: 400;
+		font-size: 15px;
+		line-height: 18px;
+		/* identical to box height */
+
+		letter-spacing: 1px;
+
+		color: rgba(176, 26, 26, 0.7);
+	}
+</style>
