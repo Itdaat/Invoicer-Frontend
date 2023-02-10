e227dc58778b07d1e08248a66bebb6e02a779c6b
Duvie-Gazda <gazda.haimdavid@gmail.com>
2023-02-10 02:08:54 +0200

    fixed bug

--- invoicer/src/lib/helpers/date.js
+++ invoicer/src/lib/helpers/date.js
@@ -5,7 +5,7 @@
  */
 export function dateToString(date) {
     const dayUnformatted = date.getDate();
-    const monthUnformatted = date.getMonth();
+    const monthUnformatted = date.getMonth() + 1;
     const day = dayUnformatted < 10 ? "0" + dayUnformatted : dayUnformatted;
     const month = monthUnformatted < 10 ? "0" + monthUnformatted : monthUnformatted;
     const year = date.getFullYear();
